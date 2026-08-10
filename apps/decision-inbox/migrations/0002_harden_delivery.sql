01fd5df54d1988850720f1159a9b42b2c055832e
pdHaku0 <pdhaku0@gmail.com>
2026-08-10 19:09:57 +0900

    Revert "Merge Decision Inbox live preview"
    This reverts commit 46e0a42df5838ad050e5fe0e01abf67b1ba3462b, reversing
    changes made to c369eed16d47f387eff6a887a712ca522eca26e2.

--- apps/decision-inbox/migrations/0002_harden_delivery.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE decision_requests ADD COLUMN callback_delivery_id TEXT;
-ALTER TABLE decision_requests ADD COLUMN callback_delivery_created_at INTEGER;
-
-CREATE UNIQUE INDEX decision_requests_callback_delivery_id
-  ON decision_requests (callback_delivery_id)
-  WHERE callback_delivery_id IS NOT NULL;
